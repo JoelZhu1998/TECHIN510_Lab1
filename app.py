a46be8a745bdeae4942d0b52900d5ed6f7248662
JoelZhu1998 <148395346+JoelZhu1998@users.noreply.github.com>
2024-03-26 16:19:10 -0700

    init

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import streamlit as st
-
 # Title of the profile page
 st.title("Hi, I am Joel :)")
 
 # Add a profile picture
-# st.image('path_to_image.jpg', width=150)
+st.image('DALL·E 2024-03-26 16.16.33 - Create a professional and gender-neutral avatar with a friendly demeanor. The avatar should have a neutral background to ensure it is versatile for va.webp', width=150)
 
 # Contact Information
 st.subheader('Contact Information')
